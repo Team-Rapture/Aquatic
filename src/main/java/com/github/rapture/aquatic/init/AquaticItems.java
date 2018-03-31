1309c190f9f41cd43bf7f42114cca9a85d1625d3
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2018-03-30 23:55:44 -0700

    sync merger

--- src/main/java/com/github/rapture/aquatic/init/AquaticItems.java
+++ src/main/java/com/github/rapture/aquatic/init/AquaticItems.java
@@ -1,28 +1,23 @@
 package com.github.rapture.aquatic.init;
 
 import com.github.rapture.aquatic.Aquatic;
-<<<<<<< HEAD
-import com.github.rapture.aquatic.item.armor.HeavyIronBoots;
-=======
 import com.github.rapture.aquatic.config.AquaticConfig;
 import com.github.rapture.aquatic.item.ItemAquaticCharm;
 import com.github.rapture.aquatic.item.ItemTesting;
->>>>>>> origin/master
+import com.github.rapture.aquatic.item.armor.HeavyIronBoots;
 import com.github.rapture.aquatic.util.RegistryCreate;
-import com.typesafe.config.ConfigRenderOptions;
 
 import net.minecraft.item.Item;
 
 @RegistryCreate(value = Item.class, modid = Aquatic.MODID)
 public class AquaticItems {
 
-<<<<<<< HEAD
+
     //register items by just creating public static final fields
     public static final Item heavy_iron_boots = new HeavyIronBoots();
 
-=======
+
 	public static final Item aquatic_charm = new ItemAquaticCharm("aquatic_charm", 1, 0, AquaticConfig.dimensionID);
 	public static final Item test = new ItemTesting("test");
-	
->>>>>>> origin/master
+
 }
