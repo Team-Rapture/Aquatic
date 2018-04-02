ef3b7b59426629cb323ddc687623447d62f19b9d
abused_master <techperson71@gmail.com>
2018-04-01 22:27:28 -0400

    Add import

--- src/main/java/com/github/rapture/aquatic/item/armor/ScubaSuit.java
+++ src/main/java/com/github/rapture/aquatic/item/armor/ScubaSuit.java
@@ -11,6 +11,7 @@ import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
