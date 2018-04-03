e2342a651b76dfbd6d2178481f22d0bf87fae394
abused_master <techperson71@gmail.com>
2018-04-02 23:54:23 -0400

    fixes

--- src/main/java/com/github/rapture/aquatic/block/BlockOxygenStone.java
+++ src/main/java/com/github/rapture/aquatic/block/BlockOxygenStone.java
@@ -1,17 +1,43 @@
 package com.github.rapture.aquatic.block;
 
 import com.github.rapture.aquatic.block.util.BlockRenderInLiquidBase;
+import com.github.rapture.aquatic.entity.misc.EntityWaterBubble;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.Random;
 
 public class BlockOxygenStone extends BlockRenderInLiquidBase {
 
+    public int bubbleTimer = 0;
+
     public BlockOxygenStone() {
         super("oxygen_stone");
+    }
+
+    @Override
+    public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+        List<EntityPlayer> players = world.getEntitiesWithinAABB(EntityPlayer.class, getRadius(pos, 2, 6));
+        if(players.size() > 0) {
+            for (EntityPlayer player : players) {
+                player.setAir(300);
+            }
+        }
+        super.updateTick(world, pos, state, rand);
+    }
+
+    public AxisAlignedBB getRadius(BlockPos blockPos, int size, int height) {
+        double hs = size / 2;
+        double hh = height / 2;
+        return new AxisAlignedBB(blockPos.getX() - hs, blockPos.getY() - hh, blockPos.getZ() - hs,
+                blockPos.getX() + hs, blockPos.getY() + hh, blockPos.getZ() + hs);
     }
 
     @Override
