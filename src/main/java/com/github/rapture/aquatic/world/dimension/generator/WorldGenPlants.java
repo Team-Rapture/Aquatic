839159535c500b9d2bd66df6f5d30a228ac03ba8
UpcraftLP <j-schirra@gmx.de>
2018-04-03 16:12:52 +0200

    fixed plant gen

--- src/main/java/com/github/rapture/aquatic/world/dimension/generator/WorldGenPlants.java
+++ src/main/java/com/github/rapture/aquatic/world/dimension/generator/WorldGenPlants.java
@@ -2,7 +2,9 @@ package com.github.rapture.aquatic.world.dimension.generator;
 
 import com.github.rapture.aquatic.init.AquaticBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
@@ -28,11 +30,10 @@ public class WorldGenPlants extends WorldGenerator {
 		for (int i = 0; i < 64; ++i) {
 			BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8), rand.nextInt(4) - rand.nextInt(4),
 					rand.nextInt(8) - rand.nextInt(8));
-		
-			if (worldIn.getBlockState(position) == AquaticBlocks.AQUA_WATER_BLOCK.getDefaultState()
-					&& (worldIn.getBlockState(position.down()) != AquaticBlocks.AQUA_WATER_BLOCK.getDefaultState()
-							&& worldIn.getBlockState(position.down()) == AquaticBlocks.AQUATIC_STONE.getDefaultState() )) {
-				worldIn.setBlockState(blockpos, this.state, 2);
+
+			IBlockState state = worldIn.getBlockState(blockpos);
+			if(state.getMaterial() == Material.WATER && worldIn.isSideSolid(blockpos.down(), EnumFacing.UP)) {
+                worldIn.setBlockState(blockpos, this.state, 2);
 			}
 		}
 
