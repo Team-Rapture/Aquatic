524ffe4829598076ca046f01b84c75a8cb2be541
abused_master <techperson71@gmail.com>
2018-04-02 23:54:37 -0400

    Fix waterbubble error

--- src/main/java/com/github/rapture/aquatic/entity/misc/EntityWaterBubble.java
+++ src/main/java/com/github/rapture/aquatic/entity/misc/EntityWaterBubble.java
@@ -1,6 +1,7 @@
 package com.github.rapture.aquatic.entity.misc;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -8,33 +9,26 @@ import net.minecraft.world.World;
 public class EntityWaterBubble extends Entity {
 
     public int deathCounter = 0;
-    public boolean oxygen = false;
 
     public EntityWaterBubble(World world) {
         super(world);
     }
 
-    public EntityWaterBubble(World world, BlockPos spawnAt, boolean isOxygenStone) {
+    public EntityWaterBubble(World world, BlockPos spawnAt) {
         super(world);
         this.posX = spawnAt.getX();
         this.posY = spawnAt.getY();
         this.posZ = spawnAt.getZ();
-        this.oxygen = isOxygenStone;
         this.setEntityInvulnerable(true);
     }
 
     @Override
     public void onUpdate() {
         super.onUpdate();
-        if(!oxygen) {
-            deathCounter++;
-            if (deathCounter >= 20 * 10) {
-                this.setDead();
-                deathCounter = 0;
-            }
-        }else {
-            posY++;
-            if(prevPosY)
+        deathCounter++;
+        if (deathCounter >= 20 * 10) {
+            this.setDead();
+            deathCounter = 0;
         }
     }
 
