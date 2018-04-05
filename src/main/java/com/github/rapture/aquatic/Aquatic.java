49c7537698f06897c922459857cb586be7019846
UpcraftLP <j-schirra@gmx.de>
2018-04-05 12:42:47 +0200

    moved clientside code to clientside class

--- src/main/java/com/github/rapture/aquatic/Aquatic.java
+++ src/main/java/com/github/rapture/aquatic/Aquatic.java
@@ -1,13 +1,8 @@
 package com.github.rapture.aquatic;
 
 import com.github.rapture.aquatic.creativetab.CreativeTab;
-import com.github.rapture.aquatic.init.AquaticBlocks;
-import com.github.rapture.aquatic.network.EventHandler;
 import com.github.rapture.aquatic.proxy.CommonProxy;
-import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Items;
-import net.minecraftforge.client.event.EntityViewRenderEvent;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
@@ -16,9 +11,6 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -47,6 +39,7 @@ public class Aquatic {
 
     static {
         FluidRegistry.enableUniversalBucket();
+        log.debug("Enabled Forge Universal Bucket!");
     }
 
     public static Logger getLogger() {
@@ -57,8 +50,6 @@ public class Aquatic {
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit(event);
         CREATIVE_TAB.setIcon(Items.PRISMARINE_SHARD);
-        MinecraftForge.EVENT_BUS.register(this);
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 
     @Mod.EventHandler
@@ -77,39 +68,5 @@ public class Aquatic {
     public void serverLoad(FMLServerStartingEvent event) {
         proxy.serverStarting(event);
         getLogger().info("World initialization complete.");
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void onFogRender(EntityViewRenderEvent.FogDensity e) {
-        if (e.getState().getBlock() == AquaticBlocks.AQUA_WATER_BLOCK) {
-            //GlStateManager.setFog(GlStateManager.FogMode.EXP);
-            GlStateManager.setFog(GlStateManager.FogMode.LINEAR);
-            e.setDensity(0.06f);
-            GlStateManager.setFogStart(30f);
-            GlStateManager.setFogEnd(70f);
-            e.setCanceled(true);
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void onFogRender(EntityViewRenderEvent.RenderFogEvent e) {
-        /*if (e.getState().getBlock() == AquaticBlocks.AQUA_WATER_BLOCK) {
-            GlStateManager.setFogStart(20.0f);
-            GlStateManager.setFogEnd(30.0f);
-            //e.setCanceled(true);
-        }*/
-    }
-
-    @SideOnly(Side.CLIENT)
-    @SubscribeEvent
-    public void onFogRender(EntityViewRenderEvent.FogColors e) {
-        if (e.getState().getBlock() == AquaticBlocks.AQUA_WATER_BLOCK) {
-            e.setRed(1f / 255f);
-            e.setGreen(36f / 255f);
-            e.setBlue(92f / 255f);
-            //e.setCanceled(true);
-        }
     }
 }
