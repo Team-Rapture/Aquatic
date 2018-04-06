89332a5c0992f530148db0a5705862f7aa8f2b81
EPIICTHUNDERCAT <rvillalobos102299@gmail.com>
2018-04-05 19:46:42 -0700

    stop formatting my code

--- src/main/java/com/github/rapture/aquatic/init/AquaticBlocks.java
+++ src/main/java/com/github/rapture/aquatic/init/AquaticBlocks.java
@@ -23,37 +23,34 @@ import net.minecraft.block.material.Material;
 @RegistryCreate(value = Block.class, modid = Aquatic.MODID)
 public class AquaticBlocks {
 
-    public static final Block AQUATIC_CHARM = new BlockAquaCharm("aquatic_charm", Material.CLAY);
+	public static final Block PISTIA = new BlockPistia();
+	public static final Block AQUANET_CONTROLLER = new AquaNetController();
+	public static final Block AQUA_NODE = new AquaNode();
+	public static final Block OXYGEN_STONE = new BlockOxygenStone();
+	public static final Block AQUA_WATER_BLOCK = new FluidAquaWaterBlock();
+	public static final Block SOLUTION_TANK = new BlockSolutionTank();
+	public static final Block OXYGEN_FILLER = new BlockOxygenFiller();
+	public static final Block ENERGY_FILLER = new BlockEnergyFiller();
+	public static final Block DEPTH_GENERATOR = new BlockDepthGenerator();
+	public static final Block CREATIVE_GENERATOR = new BlockCreativeGenerator();
+	public static final Block ANGLER_TORCH = new BlockAnglerTorch();
 
-    public static final Block HYDRILLA = new BlockPlantBase("hydrilla");
-    public static final Block PISTIA = new BlockPistia();
-    public static final Block AQUANET_CONTROLLER = new AquaNetController();
-    public static final Block AQUA_NODE = new AquaNode();
-    public static final Block OXYGEN_STONE = new BlockOxygenStone();
-    public static final Block AQUA_WATER_BLOCK = new FluidAquaWaterBlock();
-    public static final Block AQUATIC_STONE = new BlockBase("aquatic_stone");
-    public static final Block AQUATIC_STONE_CRACKED = new BlockBase("aquatic_stone_cracked");
-    public static final Block SOLUTION_TANK = new BlockSolutionTank();
-    public static final Block OXYGEN_FILLER = new BlockOxygenFiller();
-    public static final Block ENERGY_FILLER = new BlockEnergyFiller();
-    public static final Block DEPTH_GENERATOR = new BlockDepthGenerator();
-    public static final Block CREATIVE_GENERATOR = new BlockCreativeGenerator();
-
-    public static final Block IRON_ORE_DEPOSIT = new BlockBase("iron_ore_deposit");
-    public static final Block GOLD_ORE_DEPOSIT = new BlockBase("gold_ore_deposit");
-    public static final Block DIAMOND_ORE_DEPOSIT = new BlockBase("diamond_ore_deposit");
-    public static final Block COAL_ORE_DEPOSIT = new BlockBase("coal_ore_deposit");
-    public static final Block REDSTONE_ORE_DEPOSIT = new BlockBase("redstone_ore_deposit");
-    public static final Block LAPIZ_ORE_DEPOSIT = new BlockBase("lapiz_ore_deposit");
-    public static final Block QUARTZ_ORE_DEPOSIT = new BlockBase("quartz_ore_deposit");
-    public static final Block EMERALD_ORE_DEPOSIT = new BlockBase("emerald_ore_deposit");
-
-    public static final Block CORAL_REEF_PINK = new BlockPlantBase("coral_reef_pink");
-    public static final Block CORAL_REEF_BLUE = new BlockPlantBase("coral_reef_blue");
-    public static final Block CORAL_REEF_GREEN = new BlockPlantBase("coral_reef_green");
-    public static final Block CORAL_REEF_RED = new BlockPlantBase("coral_reef_red");
-    public static final Block CORAL_REEF_YELLOW = new BlockPlantBase("coral_reef_yellow");
-
-    public static final Block ANGLER_TORCH = new BlockAnglerTorch();
+	public static final Block IRON_ORE_DEPOSIT = new BlockBase("iron_ore_deposit");
+	public static final Block GOLD_ORE_DEPOSIT = new BlockBase("gold_ore_deposit");
+	public static final Block DIAMOND_ORE_DEPOSIT = new BlockBase("diamond_ore_deposit");
+	public static final Block COAL_ORE_DEPOSIT = new BlockBase("coal_ore_deposit");
+	public static final Block REDSTONE_ORE_DEPOSIT = new BlockBase("redstone_ore_deposit");
+	public static final Block LAPIZ_ORE_DEPOSIT = new BlockBase("lapiz_ore_deposit");
+	public static final Block QUARTZ_ORE_DEPOSIT = new BlockBase("quartz_ore_deposit");
+	public static final Block EMERALD_ORE_DEPOSIT = new BlockBase("emerald_ore_deposit");
+	public static final Block AQUATIC_STONE = new BlockBase("aquatic_stone");
+	public static final Block AQUATIC_STONE_CRACKED = new BlockBase("aquatic_stone_cracked");
+	public static final Block AQUATIC_CHARM = new BlockAquaCharm("aquatic_charm", Material.CLAY);
+	public static final Block HYDRILLA = new BlockPlantBase("hydrilla");
+	public static final Block CORAL_REEF_PINK = new BlockPlantBase("coral_reef_pink");
+	public static final Block CORAL_REEF_BLUE = new BlockPlantBase("coral_reef_blue");
+	public static final Block CORAL_REEF_GREEN = new BlockPlantBase("coral_reef_green");
+	public static final Block CORAL_REEF_RED = new BlockPlantBase("coral_reef_red");
+	public static final Block CORAL_REEF_YELLOW = new BlockPlantBase("coral_reef_yellow");
 
 }
