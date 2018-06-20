66d8231324482e28d43fb8c8ce577971dd7578f4
UpcraftLP <j-schirra@gmx.de>
2018-06-20 18:34:42 +0200

    added biome types to aquatic biome

--- src/main/java/com/github/teamrapture/aquatic/init/AquaticBiomes.java
+++ src/main/java/com/github/teamrapture/aquatic/init/AquaticBiomes.java
@@ -4,9 +4,20 @@ import com.github.teamrapture.aquatic.Aquatic;
 import com.github.teamrapture.aquatic.util.RegistryCreate;
 import com.github.teamrapture.aquatic.world.biome.BiomeAquatic;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
 
 @RegistryCreate(value = Biome.class, modid = Aquatic.MODID)
 public class AquaticBiomes {
 
     public static final Biome BIOME_AQUATIC = new BiomeAquatic(new Biome.BiomeProperties("Aquatic Ocean").setRainDisabled()).setRegistryName(Aquatic.MODID, "biome_aquatic_ocean");
+
+    static {
+        //add biome types for each biome based on the description in BIomeDictionary, this also improves modded mob spawn compatibility
+
+        BiomeDictionary.addTypes(AquaticBiomes.BIOME_AQUATIC,
+                BiomeDictionary.Type.OCEAN, //WATER is automatically set
+                BiomeDictionary.Type.WET,
+                BiomeDictionary.Type.SPOOKY, BiomeDictionary.Type.MAGICAL, BiomeDictionary.Type.RARE
+        );
+    }
 }
