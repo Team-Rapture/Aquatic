fd26528dbd0232bb1814a951c938be9564d93640
UpcraftLP <j-schirra@gmx.de>
2018-06-21 03:08:49 +0200

    fog render overhaul

--- src/main/java/com/github/teamrapture/aquatic/client/RenderHandler.java
+++ src/main/java/com/github/teamrapture/aquatic/client/RenderHandler.java
@@ -5,6 +5,7 @@ import com.github.teamrapture.aquatic.init.AquaticBlocks;
 import com.github.teamrapture.aquatic.init.AquaticItems;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.RenderBlockOverlayEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -19,22 +20,13 @@ public class RenderHandler {
     @SubscribeEvent
     public static void onFogRender(EntityViewRenderEvent.FogDensity e) {
         if (e.getState().getBlock() == AquaticBlocks.AQUA_WATER_BLOCK) {
-            //GlStateManager.setFog(GlStateManager.FogMode.EXP);
             GlStateManager.setFog(GlStateManager.FogMode.LINEAR);
-            e.setDensity(0.06f);
-            GlStateManager.setFogStart(30f);
-            GlStateManager.setFogEnd(70f);
+            double partial = MathHelper.clamp(e.getEntity().posY / (e.getEntity().world.getHeight() * 0.7D), 0.0D, 1.0D);
+            e.setDensity((float) (0.06D * (1.0D - partial)));
+            GlStateManager.setFogStart((float) Math.max(60.0D * (partial * partial), 0.7D));
+            GlStateManager.setFogEnd((float) Math.max(250.0D * (partial * partial), 2.2D));
             e.setCanceled(true);
         }
-    }
-
-    @SubscribeEvent
-    public static void onFogRender(EntityViewRenderEvent.RenderFogEvent e) {
-        /*if (e.getState().getBlock() == AquaticBlocks.AQUA_WATER_BLOCK) {
-            GlStateManager.setFogStart(20.0f);
-            GlStateManager.setFogEnd(30.0f);
-            //e.setCanceled(true);
-        }*/
     }
 
     @SubscribeEvent
@@ -43,14 +35,12 @@ public class RenderHandler {
             e.setRed(1f / 255f);
             e.setGreen(36f / 255f);
             e.setBlue(92f / 255f);
-            //e.setCanceled(true);
         }
     }
 
     @SubscribeEvent
     public static void waterOverlayRender(RenderBlockOverlayEvent event) {
-        if(event.getOverlayType() == RenderBlockOverlayEvent.OverlayType.WATER
-                && event.getPlayer().getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == AquaticItems.SCUBA_HELEMT) {
+        if(event.getOverlayType() == RenderBlockOverlayEvent.OverlayType.WATER && event.getPlayer().getItemStackFromSlot(EntityEquipmentSlot.HEAD).getItem() == AquaticItems.SCUBA_HELEMT) {
             event.setCanceled(true);
         }
     }
